e0db296c7737d740d5b5e3ba04706c147dc47da5
Michael <mwhitman95@gmail.com>
2017-04-11 16:34:55 +0100

    Old changes added in. STILL BROKEN

--- pandasFullModel.py
+++ pandasFullModel.py
@@ -1,13 +1,14 @@
 import numpy as np
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.preprocessing import Imputer, OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import Imputer, OneHotEncoder, FunctionTransformer, normalize
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_selection import SelectKBest, chi2, SelectFromModel
 from sklearn_pandas import DataFrameMapper
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.naive_bayes import GaussianNB
 #improve pCTR
 #write non-linear model
 #machine learning for c and langrangian multiplier
@@ -18,19 +19,21 @@ validateFile = 'dataset/validation.csv'
 testFile = 'dataset/test.csv'
 out_file = 'dataset/testing_bidding_price.csv'
 BALANCED = None #'balanced'
-K_FEATS = 100
+K_FEATS = 50
 ZERO_MULT = 100
 C = 1e6
 BASE_BID = 50
 
-MODEL_CONST = 65
-L = 5e-4
+MODEL_CONST = 1e6
+L = .0007
 
-#For Kbest feature selection
 dv = DictVectorizer()
+#For Kbest feature selection
 kbest = SelectKBest(chi2, k=K_FEATS)
 
-lr = linear_model.LogisticRegression(C=.1, penalty='l1', dual=False) # For select from model feature selection
+
+lr = linear_model.LogisticRegression(C=2, penalty='l1', dual=False) # For select from model feature selection
+
 
 stratifier = RandomForestClassifier(n_estimators=10)
 
@@ -57,6 +60,15 @@ def getFeatures(data, fit=False):
     # Stratify useragent into os and browser
     agent_info = pd.DataFrame(np.array([[item for item in agent.split('_')] for agent in data.useragent]), columns=['os','browser'])
     data = data.drop('useragent',axis=1).join(agent_info)
+    weekend_info = pd.DataFrame(np.array([1 if 5<=int(weekday)<=7 else 0 for weekday in data.weekday]))
+    peak_info = pd.DataFrame(np.array([1 if 17<=int(hour)<=24 else 0 for hour in data.hour]))
+    area_info = pd.DataFrame(np.array([int(dim['slotwidth'])*int(dim['slotheight']) for i,dim in data.iterrows()]))
+    mobile_info = pd.DataFrame(np.array([1 if os in ['android','ios'] else 0 for os in data.os]))
+    data = data.drop(['weekday','hour','slotwidth','slotheight'],axis=1)
+    data['weekend'] = weekend_info
+    data['peak'] = peak_info
+    data['area'] = area_info
+    data['mobile'] = mobile_info
     # Keep it sparse!
     if fit:
         vec = dv.fit_transform(data.drop(metrics, axis=1).to_dict(orient='records'))
@@ -66,7 +78,12 @@ def getFeatures(data, fit=False):
         vec = dv.transform(data.drop(metrics, axis=1).to_dict(orient='records'))
     # selector = SelectFromModel(lr, prefit=True)
     # features = selector.transform(vec)
+    vec = normalize(vec, axis=0) #normalize(vec)
     features = kbest.transform(vec)
+    if fit:
+        lr.fit(features, data['click'])
+    selector = SelectFromModel(lr, prefit=True)
+    # features = selector.transform(features)
 
     # if fit:
     #     vec_df = pd.DataFrame(dv.fit_transform(data.to_dict(orient='records')).toarray())
@@ -91,9 +108,14 @@ def getFeatures(data, fit=False):
     #     features = feat_select_mapper.transform(vec_df.drop(metrics,axis=1))
     # features_df = pd.DataFrame(features, columns=vec_df.columns[kbest.get_support(indices=True)])
     # print(features_df.columns)
-    return features
+    return features.toarray()
 
-def learnPriceStrata(data, features):
+def appendPriceCategories(features, cats):
+    cats = cats.reshape(-1,1)
+    features = np.append(features,cats, axis=1)
+    return normalize(features, axis=1)
+
+def getCats(data):
     cats = []
     for price in data['payprice']:
         if price <= 60:
@@ -102,13 +124,16 @@ def learnPriceStrata(data, features):
             cats.append(1)
         else:
             cats.append(2)
-    cats = np.array(cats)
+    return np.array(cats)
+
+def learnPriceStrata(data, features):
+    cats = getCats(data)
     stratifier.fit(features, cats)
 
 def loadData(fileName, train=False, test=False):
     print('Loading data (%s)...' % fileName)
-    if not test: df = pd.read_csv(fileName, usecols=columns, dtype={'weekday':object,'hour':object,'region':object,'city':object,'slotwidth':object,'slotheight':object})
-    else: df = pd.read_csv(fileName, usecols=test_columns, dtype={'weekday':object,'hour':object,'region':object,'city':object,'slotwidth':object,'slotheight':object})
+    if not test: df = pd.read_csv(fileName, usecols=columns, dtype={'weekday':object,'hour':object,'region':object,'city':object})
+    else: df = pd.read_csv(fileName, usecols=test_columns, dtype={'weekday':object,'hour':object,'region':object,'city':object})
     print('Preprocessing (%s)...' % fileName)
     if train: df = undersample(df, zeroMult=ZERO_MULT)
     features = getFeatures(df, fit=train)
@@ -129,9 +154,10 @@ def loadData(fileName, train=False, test=False):
 #     # L = 0.006
 #     return (np.sqrt(MODEL_CONST / L * pCTR + MODEL_CONST**2) - MODEL_CONST) * mults
 
-def getpredictionORTB1(pCTR, pCat, budgetRemaining):
-    multipliers = {0: 1, 1: 1, 2: 1}
+def getpredictionORTB1(pCTR, pCat, budgetRemaining, avgCTR):
+    multipliers = {0: 1, 1: 1.5, 2: 1}
     mult = multipliers[pCat]
+    # return 50 * pCTR / avgCTR
     return (np.sqrt(MODEL_CONST / L * pCTR + MODEL_CONST**2) - MODEL_CONST) #* mult
 
     # return ((pCTR + (MODEL_CONST**2 * L**2 + pCTR**2)**(1/2)) / (MODEL_CONST * L))**(1/3) - ((MODEL_CONST * L) / (pCTR + (MODEL_CONST**2 * L**2 + pCTR**2)**(1/2)))**(1/3)
@@ -164,11 +190,12 @@ def outputTestResults(model):
 
 
 train_df, train_features = loadData(trainFile, train=True)
+cats_train = getCats(train_df)
 avgCTR = train_df['click'].mean() # Average ctr of reduced (if performed) data set
 print('Learning...')
 model = linear_model.LogisticRegression(n_jobs=-1, C=C, class_weight=BALANCED)
 # model = linear_model.LogisticRegressionCV(n_jobs=-1, class_weight=BALANCED, Cs=, cv=10, penalty='l2', dual=False, refit=True, multi_class='ovr', solver='liblinear')
-model.fit(train_features, train_df.click)
+model.fit(appendPriceCategories(train_features, cats_train), train_df.click)
 learnPriceStrata(train_df, train_features)
 
 # outputTestResults(model)
@@ -178,7 +205,13 @@ print('Evaluating...')
 val_df, val_features = loadData(validateFile, train=False)
 
 pCats = stratifier.predict(val_features)
-pCTR = model.predict_proba(val_features)[:, 1]
+# pCTR = model.predict_proba(train_features)[:, 1]
+# pCTR_click = pCTR[train_df['click'] == 1]
+# pCTR_nonclick = pCTR[train_df['click'] == 0]
+pCTR = model.predict_proba(appendPriceCategories(val_features, pCats))[:, 1]
+# plt.hist(pCTR)
+# plt.axis([0,.02,0,300000])
+# plt.show()
 pCTR_click = pCTR[val_df['click'] == 1]
 pCTR_nonclick = pCTR[val_df['click'] == 0]
 print('Average Click pCTR: %f' % np.average(pCTR_click))
@@ -187,15 +220,13 @@ print('Average Nonclick pCTR: %f' %np.average(pCTR_nonclick))
 print('Median Nonclick pCTR: %f' % np.median(pCTR_nonclick))
 # sys.exit()
 
-# guesses = getPredictionsORTB1(model, val_features)
-# print(guesses)
 bidsPlaced = 0
 numWins = 0
 clicks = 0
 spent = 0
 clicksMissed = 0
 for i in range(len(pCTR)):
-    bidAmt = getpredictionORTB1(pCTR[i], pCats[i], 25000-spent)
+    bidAmt = getpredictionORTB1(pCTR[i], pCats[i], 25000-spent, avgCTR)
     bid = val_df.iloc[i]
     if (spent + bidAmt) <= 25000: #Would not place bid if the bid amount surpasses the budget
         bidsPlaced += 1
